6869f2c6ee1f6c9a8655612fe7dfbf25cb52c7d1
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-03-24 09:39:56 +0100

    Create users, rooms, bookings and reviews

--- src/seed.ts
+++ src/seed.ts
@@ -40,6 +40,12 @@ const imgRoom = [
     "https://i.pinimg.com/564x/33/a2/ef/33a2ef13fd9331064dc309b763475998.jpg"
 ]
 
+const note = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+]
+
 function createRandomUser(): User {
     return {
         id: faker.datatype.number({ max: 999 }),
@@ -78,8 +84,8 @@ function createRandomBooking(): Booking {
         checkinTime: '',
         checkoutDate: '',
         checkoutTime: '',
-        note: '',
-        type: '',
+        note: faker.helpers.arrayElement(note),
+        type: faker.helpers.arrayElement(['Double Bed', 'Single Bed', 'Double Superior']),
         status: faker.helpers.arrayElement(['Booked', 'Refund', 'Progress']),
     };
 }
@@ -91,8 +97,8 @@ function createRandomReview(): Review {
         customer: faker.name.fullName(),
         email: faker.internet.email(),
         phone: faker.phone.number('+34 ### ### ###'),
-        affair: '',
-        comment: '',
+        affair: "recommendation",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         archived: true
     };
 }
